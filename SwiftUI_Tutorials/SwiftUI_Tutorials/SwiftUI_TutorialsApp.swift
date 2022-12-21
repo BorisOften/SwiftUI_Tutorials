c295af3a4535103a0f55a8219535b11e281016b2
Boris025 <98716155+Boris025@users.noreply.github.com>
2022-12-21 07:45:21 -0600

    TAsk: - first commit

---
+++ SwiftUI_Tutorials/SwiftUI_Tutorials/SwiftUI_TutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TutorialsApp.swift
+//  SwiftUI_Tutorials
+//
+//  Created by Boris Ofon on 12/21/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
